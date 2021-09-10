ea059b01b19883e54e6ddb171f05fe359994fdbf
Wiziard38 <mathis.girault38@gmail.com>
2021-09-10 21:26:56 +0200

    joeur ordi

--- Morpion/joueur_ordi.py
+++ Morpion/joueur_ordi.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import morpion
+import random
 
 def joue_coup(cases, symbole):
     """
-    Fonction qui va faire jouer l'ordinateur pour obtenir le meilleur coup possible au morpion.
+    Fonction qui va faire jouer l'ordinateur pour obtenir un coup au hasard au morpion.
     """
     
-    return # num_case
+    while True:
+        num_case = random.randint(0,8)
+        if int(cases[num_case]) == num_case:
+            return # num_case
