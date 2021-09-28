e8b327a9a337c74f67f6f45c81015b048f4a47bc
Mathis Girault <mathis.girault@grenoble-inp.org>
2021-09-28 22:14:19 +0200

    maj

--- ressources.py
+++ ressources.py
@@ -16,62 +16,82 @@ EnsembleRessources = namedtuple("EnsembleRessources",
                                 "intervalles, nb_ressources")
 
 def cree_ensemble_ressource(nb_ressources):
-    """Créé un ensemble de ressources de taille nb_ressources.
+      """Créé un ensemble de ressources de taille nb_ressources.
 
-    L'ensemble est représenté par un namedtuple EnsembleRessources.
+      L'ensemble est représenté par un namedtuple EnsembleRessources.
 
-    L'ensemble créé contient toutes les ressources avec un identifiant id
-    tel que 0 <= id < nb_ressources.
-    """
-    # TODO
-    ...
+      L'ensemble créé contient toutes les ressources avec un identifiant id
+      tel que 0 <= id < nb_ressources.
+      """
+      
+      return EnsembleRessources([[0,nb_ressources]], nb_ressources)
 
 def contient(ensemble_ressources, identifiant):
-    """ Test d'appartenance d'une ressource à un ensemble.
+      """ Test d'appartenance d'une ressource à un ensemble.
 
-    Renvoie True si la ressource identifiée par l'identifiant donné
-    est contenu dans ensemble_ressources et False sinon.
-    """
-    # TODO
-    ...
+      Renvoie True si la ressource identifiée par l'identifiant donné
+      est contenu dans ensemble_ressources et False sinon.
+      """
+      
+      chaine = get_chaine(ensemble_ressources)
+      if chaine[identifiant] == 'x':
+            return True
+      return False
+      
 
 def get_chaine(ensemble_ressources):
-    """Renvoie une chaîne de caractère représentant l'ensemble donné".
+      """Renvoie une chaîne de caractère représentant l'ensemble donné".
 
-    Par exemple, '|x..xxxxx..|' indique qu'il y a 10 ressources,
-    et que les ressources 0, 3, 4, 5, 6 et 7 sont contenues dans l'ensemble.
-    """
-    # TODO
-    ...
+      Par exemple, '|x..xxxxx..|' indique qu'il y a 10 ressources,
+      et que les ressources 0, 3, 4, 5, 6 et 7 sont contenues dans l'ensemble.
+      """
+    
+      chaine = '|'
+      i = 0
+      while i != ensemble_ressources.nb_ressources:
+            for interval in ensemble_ressources.intervalles:
+                  for k in range(interval[0], interval[1]):
+                        while i < k:
+                              chaine += '.'
+                              i += 1
+                        chaine += 'x'
+                        i += 1
+            while i < ensemble_ressources.nb_ressources:
+                  chaine += '.'
+                  i += 1
+            chaine += '|'
+      return chaine
+
 
 def ajoute(ensemble_ressources, ensemble_ressources_a_ajouter):
-    """Ajoute des ressources précédemment enlevées dans un ensemble.
+      """Ajoute des ressources précédemment enlevées dans un ensemble.
 
-    Ajoute toutes les ressources de ensemble_ressources_a_ajouter dans
-    l'ensemble ensemble_ressources.
+      Ajoute toutes les ressources de ensemble_ressources_a_ajouter dans
+      l'ensemble ensemble_ressources.
 
-    Le fait que les ressources ajoutées aient été précédemment enlevées
-    implique qu'aucune des ressources à ajouter ne soit déjà présente dans
-    ensemble_ressources.
+      Le fait que les ressources ajoutées aient été précédemment enlevées
+      implique qu'aucune des ressources à ajouter ne soit déjà présente dans
+      ensemble_ressources.
 
-    Enfin, les deux ensembles de ressources ont le même nb_ressources et les
-    tableaux dynamiques d'intervalles de ces deux ressources sont triés.
-    """
-    # TODO
-    ...
+      Enfin, les deux ensembles de ressources ont le même nb_ressources et les
+      tableaux dynamiques d'intervalles de ces deux ressources sont triés.
+      """
+      # TODO
+      ...
 
 def enleve(ensemble_ressources, nb_ressources):
-    """Enlève nb_ressources de l'ensemble donnée.
+      """Enlève nb_ressources de l'ensemble donnée.
 
-    Les ressources enlevées sont les nb_ressources *premières ressources* de
-    l'ensemble donné.
+      Les ressources enlevées sont les nb_ressources *premières ressources* de
+      l'ensemble donné.
 
-    Cette fonction *doit renvoyer* un nouvel ensemble de ressources de même
-    taille que l'ensemble donné contenant uniquement les ressources qui ont
-    été enlevées.
-    """
-    # TODO
-    ...
+      Cette fonction *doit renvoyer* un nouvel ensemble de ressources de même
+      taille que l'ensemble donné contenant uniquement les ressources qui ont
+      été enlevées.
+      """
+      # TODO
+      ...
+
 
 def test():
     """On teste en gruyerisant un ensemble de ressources"""
