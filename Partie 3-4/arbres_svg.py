300c9034e022908e5886aee34ffdc8c71461bbaa
Mathis Girault <mathis.girault@grenoble-inp.org>
2021-12-18 22:40:30 +0100

    done arbre

--- Partie 3-4/arbres_svg.py
+++ Partie 3-4/arbres_svg.py
@@ -2,29 +2,43 @@
 """ Module python permettant le tracé d'un arbre en SVG. """
 
 from random import randint, random
-from math import sqrt, cos, sin, pi
+from math import sqrt, cos, sin, pi, atan
 
 import svg
 
 def distance(point1, point2):
     """ Fonction qui renvoie la distance carthésienne. """
-    return (point1.x - point2.x)**2 + (point1.y - point2.y)**2
+    return sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
 
 def deplacement(point, angle, longueur):
     """ Fonction qui translate un point dans l'espace. """
-    return svg.Point(point.x + sin(angle)*longueur, point.y - cos(angle)*longueur)
+    return svg.Point(point.x + cos(angle)*longueur, point.y - sin(angle)*longueur)
+
+def recup_angle(point1, point2):
+    """ Fonction qui renvoie l'angle entre 2 points et la verticale. """
+    if point1.x == point2.x:
+        return pi/2
+    if point1.x < point2.x:
+        return atan((point1.y - point2.y)/(point2.x - point1.x))
+    return pi - atan((point1.y - point2.y)/(point1.x - point2.x))
 
 def genere_branche(point_depart, point_arrivee, taille_limite, image):
     """ Fonction recursive qui s'occupe de la génération des branches d'un arbre. """
-    taille_branche = sqrt(distance(point_depart, point_arrivee))
-    if taille_branche > taille_limite:
-        print(svg.genere_segment(point_depart, point_arrivee), file = image)
-        nb_branches = randint(2, 4)
-        for _ in range(nb_branches):
-            angle = (random()-0.5)*pi # Un angle entre -pi et pi
-            longueur = random()*taille_branche
-            genere_branche(point_arrivee, deplacement(point_arrivee, angle, longueur), \
-                taille_limite, image)
+    taille_branche = distance(point_depart, point_arrivee)
+    if taille_branche < taille_limite:
+        return
+    if point_arrivee.y < 50 or point_arrivee.x < 50 or point_arrivee.x > 750:
+        return
+
+    print(svg.genere_segment(point_depart, point_arrivee), file = image)
+    nb_branches = randint(2, 4)
+    for _ in range(nb_branches):
+        ancien_angle = recup_angle(point_depart, point_arrivee)
+        nouveau_depart = deplacement(point_depart, ancien_angle, (1-random()/3)*taille_branche)
+        nouvel_angle = (random()-0.5)*2/3*pi + ancien_angle
+        longueur = random()*taille_branche
+        nouvelle_arrivee = deplacement(nouveau_depart, nouvel_angle, longueur)
+        genere_branche(nouveau_depart, nouvelle_arrivee, taille_limite, image)
 
 def main():
     """ Fonction centrale qui va gérer la création de l'arbre. """
